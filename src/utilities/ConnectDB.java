18dfaba4a964d53ab047f26a5c4a3b9bc848092f
Albert Ibanez <ait98@hotmail.com>
2018-01-11 17:18:29 +0100

    Patron Singleton Implementado (Marta la mejor profesora, ponme un 10)

---
+++ src/utilities/ConnectDB.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilities;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Albert
+ */
+public class ConnectDB {
+    
+    private static Connection instance;
+    
+    private static Connection getInstance() throws SQLException {
+    
+        if (instance==null) {
+            instance = DriverManager.getConnection(MYSQLConnection.URL,MYSQLConnection.USERNAME,MYSQLConnection.PASSWD);
+        }
+        return instance;
+    }
+    
+    public void closeConnection() throws SQLException{
+        if (instance!=null) {
+            instance.close();
+            instance = null;
+        }
+    }
+    
+}
---
+++ src/utilities/MYSQLConnection.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilities;
+
+/**
+ *
+ * @author Albert
+ */
+public class MYSQLConnection {
+    
+    public static final String URL= "jdbc:mysql://localhost:3306/avalon";
+    public static final String USERNAME = "root";
+    public static final String PASSWD = "";
+    
+    
+}
